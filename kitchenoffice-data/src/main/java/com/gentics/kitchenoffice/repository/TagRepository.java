10825f87717304c5cb73cca39c3925aae0fa579b
Leonard Osang <l.osang@gentics.com>
2012-12-05 18:04:03 +0100

    cleaned up repositories

--- kitchenoffice-data/src/main/java/com/gentics/kitchenoffice/repository/TagRepository.java
+++ kitchenoffice-data/src/main/java/com/gentics/kitchenoffice/repository/TagRepository.java
@@ -2,7 +2,6 @@ package com.gentics.kitchenoffice.repository;
 
 import org.springframework.data.neo4j.repository.GraphRepository;
 
-import com.gentics.kitchenoffice.data.Article;
 import com.gentics.kitchenoffice.data.Tag;
 
 public interface TagRepository extends GraphRepository<Tag> {}
