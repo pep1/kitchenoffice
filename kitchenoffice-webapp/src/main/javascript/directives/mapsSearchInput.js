70b8509263bba5fb59900f487471ab74111624cf
Leonard Osang <leo.osang@gmail.com>
2013-06-18 23:45:19 +0200

    Add drag support to maps

--- kitchenoffice-webapp/src/main/javascript/directives/mapsSearchInput.js
+++ kitchenoffice-webapp/src/main/javascript/directives/mapsSearchInput.js
@@ -7,17 +7,35 @@ function(location, $timeout) {
 			$timeout(function(){
 				
 				$scope.locationMap;
+				$scope.isPlace = false;
 
                 var autocomplete = new google.maps.places.Autocomplete($element[0]);
                 
                 $scope.marker = new google.maps.Marker({
-                    map: $scope.locationMap
-                  });
+                    map: $scope.locationMap,
+                    position: $scope.locationMap.center,
+                    draggable: true
+                });
                 
                 $scope.infowindow = new google.maps.InfoWindow();
                 
                 autocomplete.bindTo('bounds', $scope.locationMap);
                 
+                // when the user drags the marker
+                google.maps.event.addListener($scope.marker, 'dragend', function() {
+                	$scope.event.location.latitude = $scope.marker.getPosition().lat();
+        			$scope.event.location.longitude = $scope.marker.getPosition().lng();
+        			
+        			// if previously searched a place, truncate all inputs
+        			if($scope.isPlace) {
+        				$scope.isPlace = false;
+        				$scope.event.location.name = "";
+            			$scope.event.location.address = "";
+            			$scope.event.location.website = "";
+        			};
+                });
+                
+                // if the user did some input to the search field
                 google.maps.event.addListener(autocomplete, 'place_changed', function() {
       			  var place = autocomplete.getPlace();
       			  
@@ -31,11 +49,15 @@ function(location, $timeout) {
       				$scope.locationMap.setCenter(place.geometry.location);
       				$scope.locationMap.setZoom(17);
       			  }
-      			  var image = new google.maps.MarkerImage(
-      			      place.icon, new google.maps.Size(71, 71),
-      			      new google.maps.Point(0, 0), new google.maps.Point(17, 34),
-      			      new google.maps.Size(35, 35));
-      			  $scope.marker.setIcon(image);
+      			  
+//      			  if(place.icon) {
+//	  				 var image = new google.maps.MarkerImage(
+//	  	      			  place.icon, new google.maps.Size(71, 71),
+//	  	      			  new google.maps.Point(0, 0), new google.maps.Point(17, 34),
+//	  	      			  new google.maps.Size(35, 35));
+//	  				 $scope.marker.setIcon(image);
+//      			  }
+      			  
       			  $scope.marker.setPosition(place.geometry.location);
 
       			  $scope.infowindow.setContent(place.name);
@@ -45,8 +67,11 @@ function(location, $timeout) {
       			  $scope.event.location.address = place.formatted_address;
       			  $scope.event.location.website = place.website;
       			  
-      			  $scope.event.location.latitude = place.geometry.location.lat();
-      			  $scope.event.location.longitude = place.geometry.location.lng();
+      			  $scope.event.location.latitude = $scope.marker.getPosition().lat();
+      			  $scope.event.location.longitude = $scope.marker.getPosition().lng();
+      			  
+      			  // remember that this was found by google place
+      			  $scope.isPlace = true;
       			});
                 
             }, 50);
