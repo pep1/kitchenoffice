79084c526d6977108484bdc895aa79e45f240ade
Leonard Osang <l.osang@gentics.com>
2013-11-25 15:06:31 +0100

    Added automated SHA-1 naming for assets files

---
+++ kitchenoffice-api/src/main/java/com/gentics/kitchenoffice/taglib/WroResourceTag.java
@@ -0,0 +1,73 @@
+package com.gentics.kitchenoffice.taglib;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+
+public class WroResourceTag extends TagSupport {
+
+	private static final String WROMAPPING_PROPERTIES_FILENAME = "wromapping.properties";
+
+	private static Properties wroMapping;
+
+	private String resource;
+
+	private String path;
+
+	@Override
+	public int doStartTag() throws JspException {
+
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			String contextPath = pageContext.getServletContext().getContextPath();
+			
+			sb.append(contextPath);
+			sb.append(path);
+
+			if (!path.endsWith("/")) {
+				sb.append('/');
+			}
+			sb.append(getWroMapping().getProperty(resource, resource));
+			pageContext.getOut().write(sb.toString());
+		} catch (IOException io) {
+			throw new JspException(io);
+		}
+		return super.doStartTag();
+	}
+
+	private Properties getWroMapping() throws IOException {
+		if (wroMapping == null) {
+			wroMapping = new Properties();
+			String wroMappingFile = WROMAPPING_PROPERTIES_FILENAME;
+			InputStream inputStream = pageContext.getServletContext().getResourceAsStream("/WEB-INF/" + wroMappingFile);
+			if (inputStream == null) {
+				throw new FileNotFoundException("property file '" + wroMappingFile + "' not found in the classpath");
+			}
+			wroMapping.load(inputStream);
+		}
+		return wroMapping;
+	}
+
+	public String getResource() {
+		return resource;
+	}
+
+	public void setResource(String resource) {
+		this.resource = resource;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+}
--- kitchenoffice-webapp/src/main/resources/jsp/include/footer.jsp
+++ kitchenoffice-webapp/src/main/resources/jsp/include/footer.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" pageEncoding="UTF-8"%>
+<%@ taglib prefix="wro" uri="/WEB-INF/classes/tld/wroResource.tld"%>
 	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
-	<script type="text/javascript" src="/${project.build.finalName}/assets/js/vendor.js?v=${project.version}"></script>
-	<script type="text/javascript" src="/${project.build.finalName}/assets/js/app.js?v=${project.version}"></script>
+	<script type="text/javascript" src="<wro:resource path='/assets/js' resource='vendor.js'/>"></script>
+	<script type="text/javascript" src="<wro:resource path='/assets/js' resource='app.js'/>"></script>
 </body>
 </html>
