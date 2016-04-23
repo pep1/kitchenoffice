362027ea8973e8c05a7a62c46bef6adf85da215e
Leonard Osang <leonard.osang@audi.de>
2016-04-23 12:27:42 +0200

    Switch to https

--- kitchenoffice-webapp/src/main/javascript/vendor/directives/gravatar-directive.js
+++ kitchenoffice-webapp/src/main/javascript/vendor/directives/gravatar-directive.js
@@ -14,7 +14,7 @@ angular.module('ui-gravatar', ['md5']).
             getImageSrc : function(value, size, rating, defaultUrl, secure) {
                 // convert the value to lower case and then to a md5 hash
                 var hash = md5.createHash(value.toLowerCase());
-                var src = (secure ? 'https://secure' : 'http://www' ) + '.gravatar.com/avatar/' + hash + '?s=' + size + '&r=' + rating + '&d=' + defaultUrl;
+                var src = 'https://secure.gravatar.com/avatar/' + hash + '?s=' + size + '&r=' + rating + '&d=' + defaultUrl;
                 return src;
             }
         };
